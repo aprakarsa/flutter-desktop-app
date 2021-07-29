5ef5051b541cfd14bea7a4557ca999afae786573
Aditya Prakarsa <aprakarsa@gmail.com>
2021-07-29 23:17:02 +0800

    fist commit

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = '38a99cb933d59957f459';
+const githubClientSecret = 'b44b2aaae2a3ba6ea6bed043b11b21dc7fe1cd64';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
